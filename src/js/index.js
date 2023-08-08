390c0c2ef3318f7635594e340f80c29da87918a5
Mpineiro20 <130186047+Mpineiro20@users.noreply.github.com>
2023-08-08 14:24:47 +0000

    first-commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,37 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
 
-// include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+function SimpleCounter(props) {
+  return (
+    <div className="theCounter">
+      <div className="clock">
+        <i className="fas fa-clock"></i>
+      </div>
+      <div className="hours">{props.digitHours}</div>
+      <div className="minutes">{props.digitMinutes}</div>
+      <div className="tenSeconds">{props.digitTensSeconds}</div>
+      <div className="seconds">{props.digitSeconds}</div>
+    </div>
+  );
+}
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0;
+setInterval(function () {
+  const hours = Math.floor(counter / 3600);
+  const minutes = Math.floor((counter % 3600) / 60);
+  const tenSeconds = Math.floor((counter % 60) / 10);
+  const seconds = counter % 10;
+  console.log(seconds, tenSeconds, minutes, hours);
+  counter++;
+  ReactDOM.render(
+    <SimpleCounter
+      digitSeconds={seconds}
+      digitTensSeconds={tenSeconds}
+      digitMinutes={minutes}
+      digitHours={hours}
+    />,
+    document.querySelector("#app")
+  );
+}, 1000);
